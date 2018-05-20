0198d66c063d34cb12421ddc8c505dab3cdb34bd
patrickjb <patrickbatey@gmail.com>
2018-05-20 16:46:07 -0400

    minor updates

--- index.js
+++ index.js
@@ -27,9 +27,34 @@ function init() {
 			fill: '0xFFFFFF',
 		});
 
+		var ctaTextStyle = new PIXI.TextStyle({
+			align : 'center',
+			fontFamily: 'uniform_roundedbold',
+			fontSize: '28px',
+			letterSpacing: -2,
+			fill: '0xFFFFFF',
+		});
+
+		var subHeadTextStyle = new PIXI.TextStyle({
+			align : 'center',
+			fontSize: '36px',
+			fontFamily:'uniform_roundedblack',
+			fill: '0xFFFFFF',
+			letterSpacing:1
+		});
+		var yourScoreTextStyle = new PIXI.TextStyle({
+			align : 'center',
+			fontSize: '100px',
+			fontFamily:'uniform_roundedultra',
+			fill: '0xFFFFFF',
+			letterSpacing: -4
+		});
 
 		return {
-			interfaceTextStyle : interfaceTextStyle
+			interfaceTextStyle : interfaceTextStyle,
+			ctaTextStyle : ctaTextStyle,
+			subHeadTextStyle : subHeadTextStyle,
+			yourScoreTextStyle : yourScoreTextStyle,
 		}
 
 	}());
@@ -40,7 +65,6 @@ function init() {
 		var interfaceHolder, heartHolder, timerHolder, scoreHolder;
 		var timerSectors, timerSectorLength, beginAngle;
 		var heart1, heart2, heart3;
-
 
 		return {
 			scoreText,
@@ -63,26 +87,96 @@ function init() {
 	}());
 
 
-
-	Sprites = (function() {
-
-		//INTRO
+	Intro = (function() {
 		var ctaBg, ctaText, ctaHolder, overlay, ahLogo, logoTextures, instructionText, cabLogo, cabCatch, cabA, cabBite, cabBg, cabCandy1, cabCandy2, cabCandy3, cabCandy4;
-		//MAIN
-		var bgholder, candyHolder, fgHolder, airheadHolder, hitRect;
+
+		return {
+			ctaBg,
+			ctaText,
+			ctaHolder,
+			overlay : overlay,
+			ahLogo,
+			logoTextures,
+			instructionText,
+			cabLogo,
+			cabCatch,
+			cabA,
+			cabBite,
+			cabBg,
+			cabCandy1,
+			cabCandy2,
+			cabCandy3,
+			cabCandy4
+		}
+
+
+	}());
+
+	Main = (function() {
+		var bgHolder, candyHolder, fgHolder, airheadHolder, hitRect;
 		var sky_bg, buildings, trees, hedges, street, lightpoles;
 		var candy0, candy1, candy2, candy3, candy4, candy5, candy6;
 		var airHead, airBody, leftArm, rightArm, leftLeg, rightLeg, torso, head, pelvis, headTextures;
 
-		//ENDFRAME
-		var endFrame, overlayEnd, endCtaBg1, endCtaBg2, endCtaHolder1, endCtaHolder2, endCtaText1, endCtaText2, ahLogoEnd, highScoreText, endSubhead;
-		var cabLogoEnd, cabCatchEnd, cabAEnd, cabBiteEnd, cabBgEnd, cabCandy1End, cabCandy2End, cabCandy3End, cabCandy4End;
-
 		return {
-			ctaBg, ctaText, ctaHolder, overlay, ahLogo, logoTextures, instructionText, cabLogo, cabCatch, cabA, cabBite, cabBg, cabCandy1, cabCandy2, cabCandy3, cabCandy4, bgholder, candyHolder, fgHolder, airheadHolder, hitRect, sky_bg, buildings, trees, hedges, street, lightpoles, candy0, candy1, candy2, candy3, candy4, candy5, candy6, airHead, airBody, leftArm, rightArm, leftLeg, rightLeg, torso, head, pelvis, headTextures, endFrame, overlayEnd, endCtaBg1, endCtaBg2, endCtaHolder1, endCtaHolder2, endCtaText1, endCtaText2, ahLogoEnd, highScoreText, endSubhead, cabLogoEnd, cabCatchEnd, cabAEnd, cabBiteEnd, cabBgEnd, cabCandy1End, cabCandy2End, cabCandy3End, cabCandy4End
+			bgHolder,
+			candyHolder,
+			fgHolder,
+			airheadHolder,
+			hitRect,
+			sky_bg,
+			buildings,
+			trees,
+			hedges,
+			street,
+			lightpoles,
+			candy0,
+			candy1,
+			candy2,
+			candy3,
+			candy4,
+			candy5,
+			candy6,
+			airHead,
+			airBody,
+			leftArm,
+			rightArm,
+			leftLeg,
+			rightLeg,
+			torso,
+			head,
+			pelvis,
+			headTextures
 		}
 	}());
 
+	EndFrame = (function() {
+		var endFrame, overlayEnd, endCtaBg1, endCtaBg2, endCtaHolder1, endCtaHolder2, endCtaText1, endCtaText2, ahLogoEnd, yourScoreText, endSubhead;
+		var cabLogoEnd, cabCatchEnd, cabAEnd, cabBiteEnd, cabBgEnd, cabCandy1End, cabCandy2End, cabCandy3End, cabCandy4End;
+
+		return {
+			endFrame,
+			overlayEnd,
+			endCtaBg1,
+			endCtaBg2,
+			endCtaHolder1,
+			endCtaHolder2,
+			endCtaText1,
+			endCtaText2,
+			ahLogoEnd,
+			yourScoreText,
+			endSubhead,
+			cabLogoEnd,
+			cabCatchEnd,
+			cabAEnd,
+			cabBiteEnd,
+			cabBgEnd,
+			cabCandy1End,
+			cabCandy2End,
+			cabCandy3End,
+			cabCandy4End
+		}
+	}());
 
 	Sounds = (function() {
 		var bgSound, flapSound, buttonSound, eatSound, loseSound, winSound, overSound;
@@ -213,38 +307,80 @@ function init() {
 	Interface.loadingText.position.set(stageW / 2 - Interface.loadingText.width / 2, stageH / 2);
 	app.stage.addChild(Interface.loadingText);
 
+	function buildStage() {
+		log('BUILD STAGE');
+
+
+		//Intro.intro.addChild(Intro.overlay);
+		Intro.intro.addChild(Intro.cabLogo);
+
+		//app.stage.addChild(Intro.overlay);
+
+
+
+	}
+
+
+	function setPosition() {
+		log('SET POSITION');
+
+
+		Intro.intro.addChild(Intro.overlay);
+		Intro.overlay.anchor.set(0.0);
+
+		Intro.intro.position.set(0, 0);
+		log(Intro.overlay.width);
+		log('HELP '  +  stageW);
+
+		app.stage.addChild(Intro.intro);
+
+		//Intro.cabLogo.position.set();
+		//Intro.ctaHolder.position.set();
+		//Intro.ahLogo.position.set();
+		//Intro.instructionText.position.set();
+
+		// Main
+
+		// EndFrame
+
+		//buildStage();
+	}
+
 	function loadProgressHandler() {
 		Interface.loadingText.setText( 'LOADING ' + Math.round(Game.loader.progress) + '%');
 	}
 
 	function setUp() {
+		log('SETUP')
+
 		//Textures
 		Sprites.logoTextures = [Game.resources['logo00.png'].texture, Game.resources['logo01.png'].texture, Game.resources['logo02.png'].texture, Game.resources['logo03.png'].texture, Game.resources['logo04.png'].texture, Game.resources['logo05.png'].texture, Game.resources['logo06.png'].texture, Game.resources['logo07.png'].texture, Game.resources['logo08.png'].texture, Game.resources['logo09.png'].texture, Game.resources['logo10.png'].texture, Game.resources['logo11.png'].texture, Game.resources['logo12.png'].texture ];
 
 		Sprites.headTextures = [Game.resources['ah_head_00.png'].texture, Game.resources['ah_head_01.png'].texture, Game.resources['ah_head_02.png'].texture, Game.resources['ah_head_02.png'].texture, Game.resources['ah_head_02.png'].texture, Game.resources['ah_head_01.png'].texture, Game.resources['ah_head_00.png'].texture];
 
 		// Intro
-		Sprites.intro 		= new PIXI.Container({width: stageW, height: stageH});
-		Sprites.cabLogo 	= new PIXI.Container();
-		Sprites.ctaHolder 	= new PIXI.Container();
-		Sprites.ahLogo  	= new PIXI.extras.AnimatedSprite(Sprites.logoTextures);
-		Sprites.cabCatch 	= new PIXI.Sprite(Game.resources['cab_catch.png'].texture);
-		Sprites.cabA 		= new PIXI.Sprite(Game.resources['cab_a.png'].texture);
-		Sprites.cabBite 	= new PIXI.Sprite(Game.resources['cab_bite.png'].texture);
-		Sprites.cabBg 		= new PIXI.Sprite(Game.resources['cab_bg.png'].texture);
-		Sprites.cabCandy1 	= new PIXI.Sprite(Game.resources['cab_candy1.png'].texture);
-		Sprites.cabCandy2 	= new PIXI.Sprite(Game.resources['cab_candy2.png'].texture);
-		Sprites.cabCandy3	= new PIXI.Sprite(Game.resources['cab_candy3.png'].texture);
-		Sprites.cabCandy4 	= new PIXI.Sprite(Game.resources['cab_candy4.png'].texture);
-		Sprites.ctaBg 		= new PIXI.Sprite(Game.resources['cta_bg.png'].texture);
-		Sprites.ctaText 	= new PIXI.Text(' Play Now ');
-		Sprites.overlay 	= new PIXI.Sprite(Game.resources['introOverlay.png'].texture);
-		Sprites.instructionText = new PIXI.Text(' Keep clicking to catch \n all the Airheads! ');
-		Sprites.instructionText.style = Text.interfaceTextStyle;
+		Intro.intro 		= new PIXI.Container();
+		Intro.cabLogo 		= new PIXI.Container();
+		Intro.ctaHolder 	= new PIXI.Container();
+		Intro.ahLogo  		= new PIXI.extras.AnimatedSprite(Sprites.logoTextures);
+		Intro.cabCatch 		= new PIXI.Sprite(Game.resources['cab_catch.png'].texture);
+		Intro.cabA 			= new PIXI.Sprite(Game.resources['cab_a.png'].texture);
+		Intro.cabBite 		= new PIXI.Sprite(Game.resources['cab_bite.png'].texture);
+		Intro.cabBg 		= new PIXI.Sprite(Game.resources['cab_bg.png'].texture);
+		Intro.cabCandy1 	= new PIXI.Sprite(Game.resources['cab_candy1.png'].texture);
+		Intro.cabCandy2 	= new PIXI.Sprite(Game.resources['cab_candy2.png'].texture);
+		Intro.cabCandy3		= new PIXI.Sprite(Game.resources['cab_candy3.png'].texture);
+		Intro.cabCandy4 	= new PIXI.Sprite(Game.resources['cab_candy4.png'].texture);
+		Intro.ctaBg 		= new PIXI.Sprite(Game.resources['cta_bg.png'].texture);
+		Intro.ctaText 		= new PIXI.Text(' Play Now ');
+		Intro.overlay 		= new PIXI.Sprite(Game.resources['introOverlay.png'].texture);
+		Intro.instructionText = new PIXI.Text(' Keep clicking to catch \n all the Airheads! ');
 
+		Intro.ctaText.style 		= Text.ctaTextStyle;
+		Intro.instructionText.style = Text.interfaceTextStyle;
 
 		// Main Game
-		Sprites.main 			= new PIXI.Container();
+		Main.main 				= new PIXI.Container();
 
 		// - interface
 		// -- score
@@ -253,103 +389,103 @@ function init() {
 		Interface.scoreText.style = Text.interfaceTextStyle;
 
 		// -- Timer
-		Interface.timerSectors = 30;
+		Interface.timerSectors 	= 30;
 		Interface.timerSectorLength = ((Math.PI / 180) * 360/ Interface.timerSectors) * 15;
-		Interface.beginAngle = 0 / Interface.timerSectors * Math.PI * 2;
+		Interface.beginAngle 	= 0 / Interface.timerSectors * Math.PI * 2;
 
-		Interface.timerText	= new PIXI.Text('00');
-		Interface.timerText.style = Text.interfaceTextStyle;
-		Interface.timerBg = new PIXI.Graphics();
-		Interface.timerIcon = new PIXI.Graphics();
+		Interface.timerText			= new PIXI.Text('00');
+		Interface.timerBg 			= new PIXI.Graphics();
+		Interface.timerIcon 		= new PIXI.Graphics();
+		Interface.timerText.style 	= Text.interfaceTextStyle;
 
 		// -- Hearts
-		Interface.heartHolder = new PIXI.Container();
-		Interface.heart1 = new PIXI.Sprite(Game.resources['heart.png'].texture);
-		Interface.heart2 = new PIXI.Sprite(Game.resources['heart.png'].texture);
-		Interface.heart3 = new PIXI.Sprite(Game.resources['heart.png'].texture);
+		Interface.heartHolder 	= new PIXI.Container();
+		Interface.heart1 		= new PIXI.Sprite(Game.resources['heart.png'].texture);
+		Interface.heart2 		= new PIXI.Sprite(Game.resources['heart.png'].texture);
+		Interface.heart3 		= new PIXI.Sprite(Game.resources['heart.png'].texture);
 
-		Sprites.hitRect = new PIXI.Graphics();
-		Sprites.hitRect.beginFill(0xFF3300);
-		Sprites.hitRect.drawRect(0, 0, stageW, stageH);
-		Sprites.hitRect.endFill();
-		Sprites.hitRect.alpha = 0.0;
-		Sprites.hitRect.interactive = true;
-		Sprites.hitRect.buttonMode = true;
-		Sprites.hitRect.hitArea = new PIXI.Rectangle(0, 0, stageW, stageH);
+		Main.hitRect = new PIXI.Graphics();
+		Main.hitRect.beginFill(0xFF3300);
+		Main.hitRect.drawRect(0, 0, stageW, stageH);
+		Main.hitRect.endFill();
+		Main.hitRect.alpha = 0.0;
+		Main.hitRect.interactive = true;
+		Main.hitRect.buttonMode = true;
+		Main.hitRect.hitArea = new PIXI.Rectangle(0, 0, stageW, stageH);
 
 		// - BackGround
-		Sprites.bgHolder 	= new PIXI.Container();
-		Sprites.sky_bg 		= new PIXI.extras.TilingSprite(Game.resources['sky_bg_@2x.jpg'].texture, stageW, stageH);
-		Sprites.buildings 	= new PIXI.extras.TilingSprite(Game.resources['buildings_@2x.png'].texture, stageW, 490 / 2);
-		Sprites.trees 		= new PIXI.extras.TilingSprite(Game.resources['trees_@2x.png'].texture, stageW, 235 / 2);
-		Sprites.hedges 		= new PIXI.extras.TilingSprite(Game.resources['hedge_@2x.png'].texture, stageW, stageH);
-		Sprites.street 		= new PIXI.extras.TilingSprite(Game.resources['road_@2x.jpg'].texture, stageW, 278 / 2);
-		Sprites.lightpoles 	= new PIXI.extras.TilingSprite(Game.resources['lightpost_foreground_@2x.png'].texture, 3600 / 2, stageH);
+		Main.bgHolder 		= new PIXI.Container();
+		Main.sky_bg 		= new PIXI.extras.TilingSprite(Game.resources['sky_bg_@2x.jpg'].texture, stageW, stageH);
+		Main.buildings 		= new PIXI.extras.TilingSprite(Game.resources['buildings_@2x.png'].texture, stageW, 490 / 2);
+		Main.trees 			= new PIXI.extras.TilingSprite(Game.resources['trees_@2x.png'].texture, stageW, 235 / 2);
+		Main.hedges 		= new PIXI.extras.TilingSprite(Game.resources['hedge_@2x.png'].texture, stageW, stageH);
+		Main.street 		= new PIXI.extras.TilingSprite(Game.resources['road_@2x.jpg'].texture, stageW, 278 / 2);
+		Main.lightpoles 	= new PIXI.extras.TilingSprite(Game.resources['lightpost_foreground_@2x.png'].texture, 3600 / 2, stageH);
 
 		// - AIRHEAD
-		Sprites.airHead = new PIXI.Container();
+		Main.airHead 		= new PIXI.Container();
 
 		// -- Head
-		Sprites.head = new PIXI.extras.AnimatedSprite(Sprites.headTextures);
-		Sprites.head.pivot.set(55, 178);
-		Sprites.head.position.set(0, 0);
-		Sprites.head.loop = false;
-		Sprites.head.animationSpeed = 0.6;
+		Main.head 			= new PIXI.extras.AnimatedSprite(Sprites.headTextures);
+		Main.head.pivot.set(55, 178);
+		Main.head.position.set(0, 0);
+		Main.head.loop = false;
+		Main.head.animationSpeed = 0.6;
 
 		// -- Body
-		Sprites.airBody = new PIXI.Container();
-		Sprites.torso = new PIXI.Sprite(Game.resources['ah_body.png'].texture);
-		Sprites.torso.pivot.set(16, 6);
-		Sprites.torso.position.set(40-55+16, 175-178+6);
-		Sprites.leftLeg = new PIXI.Sprite(Game.resources['ah_leftLeg.png'].texture);
-		Sprites.leftLeg.pivot.set(8, 4);
-		Sprites.leftLeg.position.set(48-55+8, 207-178+4);
-		Sprites.rightLeg = new PIXI.Sprite(Game.resources['ah_rightLeg.png'].texture);
-		Sprites.rightLeg.pivot.set(22, 0);
-		Sprites.rightLeg.position.set(27-55+22, 210-178+0);
-		Sprites.rightArm = new PIXI.Sprite(Game.resources['ah_rightArm.png'].texture);
-		Sprites.rightArm.pivot.set(6, 4);
-		Sprites.rightArm.position.set(43-55+6, 181-178+4);
-		Sprites.leftArm = new PIXI.Sprite(Game.resources['ah_leftArm.png'].texture);
-		Sprites.leftArm.pivot.set(0, 6);
-		Sprites.leftArm.position.set(62-55+0, 180-178+6);
+		Main.airBody 		= new PIXI.Container();
+		Main.torso 			= new PIXI.Sprite(Game.resources['ah_body.png'].texture);
+		Main.leftLeg 		= new PIXI.Sprite(Game.resources['ah_leftLeg.png'].texture);
+		Main.rightLeg 		= new PIXI.Sprite(Game.resources['ah_rightLeg.png'].texture);
+		Main.rightArm 		= new PIXI.Sprite(Game.resources['ah_rightArm.png'].texture);
+		Main.leftArm 		= new PIXI.Sprite(Game.resources['ah_leftArm.png'].texture);
+
+		Main.rightArm.pivot.set(6, 4);
+		Main.rightArm.position.set(43-55+6, 181-178+4);
+		Main.rightLeg.pivot.set(22, 0);
+		Main.rightLeg.position.set(27-55+22, 210-178+0);
+		Main.leftLeg.pivot.set(8, 4);
+		Main.leftLeg.position.set(48-55+8, 207-178+4);
+		Main.torso.pivot.set(16, 6);
+		Main.torso.position.set(40-55+16, 175-178+6);
+		Main.leftArm.pivot.set(0, 6);
+		Main.leftArm.position.set(62-55+0, 180-178+6);
 
 		// - Candy
-		Sprites.candyHolder = new PIXI.Container();
-		Sprites.candy0 = new PIXI.Sprite(Game.resources['candy_00.png'].texture);
-		Sprites.candy1 = new PIXI.Sprite(Game.resources['candy_01.png'].texture);
-		Sprites.candy2 = new PIXI.Sprite(Game.resources['candy_02.png'].texture);
-		Sprites.candy3 = new PIXI.Sprite(Game.resources['candy_03.png'].texture);
-		Sprites.candy4 = new PIXI.Sprite(Game.resources['candy_04.png'].texture);
-		Sprites.candy5 = new PIXI.Sprite(Game.resources['candy_05.png'].texture);
-		Sprites.candy6 = new PIXI.Sprite(Game.resources['candy_06.png'].texture);
+		Main.candyHolder 		= new PIXI.Container();
+		Main.candy0 			= new PIXI.Sprite(Game.resources['candy_00.png'].texture);
+		Main.candy1 			= new PIXI.Sprite(Game.resources['candy_01.png'].texture);
+		Main.candy2 			= new PIXI.Sprite(Game.resources['candy_02.png'].texture);
+		Main.candy3 			= new PIXI.Sprite(Game.resources['candy_03.png'].texture);
+		Main.candy4 			= new PIXI.Sprite(Game.resources['candy_04.png'].texture);
+		Main.candy5 			= new PIXI.Sprite(Game.resources['candy_05.png'].texture);
+		Main.candy6 			= new PIXI.Sprite(Game.resources['candy_06.png'].texture);
 
 		// Endframe
-		Sprites.endFrame = new PIXI.Container();
+		EndFrame.endFrame 		= new PIXI.Container();
+		EndFrame.yourScoreText 	= new PIXI.Text('Your score: 0');
+		EndFrame.endSubhead 	= new PIXI.Text(' Great job! ' );
+		EndFrame.cabCatchEnd  	= new PIXI.Sprite(Game.resources['cab_catch.png'].texture);
+		EndFrame.cabAEnd 	 	= new PIXI.Sprite(Game.resources['cab_a.png'].texture);
+		EndFrame.cabBiteEnd   	= new PIXI.Sprite(Game.resources['cab_bite.png'].texture);
+		EndFrame.cabBgEnd 	 	= new PIXI.Sprite(Game.resources['cab_bg.png'].texture);
+		EndFrame.cabCandy1End 	= new PIXI.Sprite(Game.resources['cab_candy1.png'].texture);
+		EndFrame.cabCandy2End 	= new PIXI.Sprite(Game.resources['cab_candy2.png'].texture);
+		EndFrame.cabCandy3End	= new PIXI.Sprite(Game.resources['cab_candy3.png'].texture);
+		EndFrame.cabCandy4End 	= new PIXI.Sprite(Game.resources['cab_candy4.png'].texture);
+		EndFrame.endCtaBg1 		= new PIXI.Sprite(Game.resources['cta_bg.png'].texture);
+		EndFrame.endCtaBg2 		= new PIXI.Sprite(Game.resources['cta_bg.png'].texture);
+		EndFrame.endCtaText1 	= new PIXI.Text(' Play Again? ');
+		EndFrame.endCtaText2 	= new PIXI.Text(' Find Near You ');
+		EndFrame.ahLogoEnd 		= new PIXI.extras.AnimatedSprite(Sprites.logoTextures);
+		EndFrame.overlayEnd 	= new PIXI.Sprite(Game.resources['endOverlay.png'].texture);
 
-		Sprites.highScoreText = new PIXI.Text('Your score: 0');
-		Sprites.endSubhead = new PIXI.Text(' Great job! ' );
+		EndFrame.yourScoreText.style 	= Text.yourScoreTextStyle;
+		EndFrame.endSubhead.style 		= Text.subHeadTextStyle;
+		EndFrame.endCtaText1.style 		= Text.ctaTextStyle;
+		EndFrame.endCtaText2.style 		= Text.ctaTextStyle;
 
-		Sprites.cabCatchEnd  = new PIXI.Sprite(Game.resources['cab_catch.png'].texture);
-		Sprites.cabAEnd 	 = new PIXI.Sprite(Game.resources['cab_a.png'].texture);
-		Sprites.cabBiteEnd   = new PIXI.Sprite(Game.resources['cab_bite.png'].texture);
-		Sprites.cabBgEnd 	 = new PIXI.Sprite(Game.resources['cab_bg.png'].texture);
-		Sprites.cabCandy1End = new PIXI.Sprite(Game.resources['cab_candy1.png'].texture);
-		Sprites.cabCandy2End = new PIXI.Sprite(Game.resources['cab_candy2.png'].texture);
-		Sprites.cabCandy3End = new PIXI.Sprite(Game.resources['cab_candy3.png'].texture);
-		Sprites.cabCandy4End = new PIXI.Sprite(Game.resources['cab_candy4.png'].texture);
-
-		Sprites.endCtaBg1 = new PIXI.Sprite(Game.resources['cta_bg.png'].texture);
-		Sprites.endCtaBg2 = new PIXI.Sprite(Game.resources['cta_bg.png'].texture);
-		Sprites.endCtaText1 = new PIXI.Text(' Play Again? ');
-		Sprites.endCtaText2 = new PIXI.Text(' Find Near You ');
-
-		Sprites.ahLogoEnd = new PIXI.extras.AnimatedSprite(Sprites.logoTextures);
-
-		Sprites.overlayEnd = new PIXI.Sprite(Game.resources['endOverlay.png'].texture);
-
-
-
+		setPosition();
 	}
 
 	Game.loader.add([
@@ -365,7 +501,7 @@ function init() {
 	log(Utils.random(21, 500));
 	log(Game._width);
 	log(Game.sinkRate);
-	log(Sprites);
+	log(Main);
 
 
 
